5e3db685b2bfc4ff2254041aaf8e3639d45751ae
Marcel Pfeiffer <pfeiffer.marcel@gmx.de>
2013-09-16 16:15:52 +0200

    TextInput: more comments; remove unused imports

--- src/textinput.d
+++ src/textinput.d
@@ -1,21 +1,22 @@
 module textinput;
 
 import observable;
-import renderhelper;
-import renderer;
-import utils;
 
 import derelict.sdl2.sdl;
 
 import std.stdio;
 import std.utf;
 
+/**
+ * a server that, when directing SDL events to it and is set reading, records
+ * what is written on the keyboard
+ **/
 class TextInput : Observable {
 	private string textInput;
 	private bool reading;
 
 	public this() {
-		super(1);
+		super(1);	// one observer allowed
 		this.reading = false;
 	}
 
@@ -55,6 +56,8 @@ class TextInput : Observable {
 				if (event.key.keysym.sym == SDLK_RETURN) {
 					this.stop();
 				} else if (event.key.keysym.sym == SDLK_BACKSPACE) {
+					// converting text to utf32 so length corresponds to number
+					// of characters
 					dstring tmp = toUTF32(this.textInput);
 					if (tmp.length > 0) {
 						tmp.length = tmp.length - 1;
