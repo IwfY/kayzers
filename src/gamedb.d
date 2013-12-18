ace9eed5d425c2813789b58ef82250c5b012b3ea
Marcel Pfeiffer <pfeiffer.marcel@gmx.de>
2013-12-18 18:22:37 +0100

    implement nation saving

--- src/gamedb.d
+++ src/gamedb.d
@@ -10,6 +10,7 @@ import game;
 import world.character;
 import world.charactermanager;
 import world.dynasty;
+import world.nation;
 
 class GameDB {
 	/**
@@ -29,9 +30,9 @@ class GameDB {
                 id INTEGER PRIMARY KEY,
                 name TEXT NOT NULL,
                 dna TEXT,
-                dynastyId INTEGER,
+                dynastyId INTEGER NOT NULL,
                 birth INTEGER NOT NULL, death INTEGER,
-                sex INTEGER,
+                sex INTEGER NOT NULL,
                 fatherId INTEGER,
                 motherId INTEGER,
                 partnerID INTEGER)"
@@ -58,7 +59,7 @@ class GameDB {
 							"NULL" : text(character.getMother().getId())),
 						((character.getPartner() is null) ?
 							"NULL" : text(character.getPartner().getId())));
-				debug(2) {
+				debug(1) {
 					import std.stdio;
 					writeln("GameDB::saveCharacters " ~ sqlCommand);
 				}
@@ -76,8 +77,8 @@ class GameDB {
 			"CREATE TABLE dynasty (
                 id INTEGER PRIMARY KEY,
                 name TEXT NOT NULL,
-                languageName TEXT,
-                flagImageName TEXT)"
+                languageName TEXT NOT NULL,
+                flagImageName TEXT NOT NULL)"
 			);
 
 		foreach(const(Dynasty) dynasty; characterManager.getDynasties()) {
@@ -90,11 +91,72 @@ class GameDB {
 					GameDB.sqlEscape(dynasty.getName()),
 					GameDB.sqlEscape(dynasty.getLanguage().getName()),
 					GameDB.sqlEscape(dynasty.getFlagImageName()));
-			debug(2) {
+			debug(1) {
 				import std.stdio;
 				writeln("GameDB::saveDynasties " ~ sqlCommand);
 			}
 			db.execute(sqlCommand);
+		}
+	}
+
+
+	private static void saveNations(Database db, const(Nation[]) nations) {
+		// create db table
+		db.execute(
+			"CREATE TABLE nation (
+                id INTEGER PRIMARY KEY,
+                prototypeName TEXT NOT NULL,
+                seatId INTEGER NOT NULL,
+                rulerId INTEGER NOT NULL,
+                colorR INTEGER NOT NULL,
+                colorG INTEGER NOT NULL,
+                colorB INTEGER NOT NULL)"
+			);
+
+		db.execute(
+			"CREATE TABLE nationResources (
+                nationId INTEGER NOT NULL,
+                resourceName TEXT NOT NULL,
+                amount FLOAT NOT NULL)"
+			);
+
+		foreach(const(Nation) nation; nations) {
+			// nation
+			string sqlCommand = "
+				INSERT INTO nation
+					(id, prototypeName, seatId, rulerId, colorR, colorG, colorB)
+				VALUES
+					(%d, \"%s\", %d, %d, %d, %d, %d)".format(
+					nation.getId(),
+					GameDB.sqlEscape(nation.getPrototype().getName()),
+					nation.getSeat().getId(),
+					nation.getRuler().getId(),
+					nation.getColor().r,
+					nation.getColor().g,
+					nation.getColor().b);
+			debug(1) {
+				import std.stdio;
+				writeln("GameDB::saveNations " ~ sqlCommand);
+			}
+			db.execute(sqlCommand);
+
+			// nation resources
+			foreach(string key, const(double) value;
+					nation.getResources().getResources()) {
+				sqlCommand = "
+					INSERT INTO nationResources
+						(nationId, resourceName, amount)
+					VALUES
+						(%d, \"%s\", %f)".format(
+						nation.getId(),
+						GameDB.sqlEscape(key),
+						value);
+				debug(1) {
+					import std.stdio;
+					writeln("GameDB::saveNations " ~ sqlCommand);
+				}
+				db.execute(sqlCommand);
+			}
 		}
 	}
 
@@ -108,6 +170,7 @@ class GameDB {
 
 		GameDB.saveDynasties(db, game.getCharacterManager());
 		GameDB.saveCharacters(db, game.getCharacterManager());
+		GameDB.saveNations(db, game.getNations());
 
 		return true;
 	}
